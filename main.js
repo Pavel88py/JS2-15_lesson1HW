26a43edb36d6f9ecfd2331fd5c1c11e0ecd86ae4
Pavel88py <z0546831707@gmail.com>
2017-11-09 18:15:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+//JS2-15 Pavel Silber
+
+// Creating the Hamburger Constructor function
+var Hamburger = function(size, stuffing) {
+    this.size = size;
+    this.stuffing = stuffing;
+
+    Hamburger.prototype.showHamburger = function () {
+        console.log(this.size + ' ' + this.stuffing + ' hamburger. Bon Appétit');
+    };
+
+    Hamburger.prototype.addTopping = function (topping) {
+        if(this.topping === 'Large') {
+            console.log('Okay!');
+        } if (this.topping === 'Small'){
+            console.log('Small hamburger');
+        } else {
+            console.log('Error');
+        }
+    };
+
+    Hamburger.prototype.removeTopping = function (topping) {
+        this.topping = null;
+    };
+};
+
+// var sizePrompt = prompt('Please type hamburger size (Large or Small');
+// var stuffingPrompt = prompt('Please type hamburger stuffing (Cheese or Salad or Potato');
+var tomasHamburger = new Hamburger('Large', 'Potato');
+
+tomasHamburger.addTopping('Small');
+
+tomasHamburger.showHamburger();
+
